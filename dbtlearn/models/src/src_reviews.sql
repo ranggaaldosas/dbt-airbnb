67d86468942c796779b4075cfbe519d71d735b27
Rangga <ranggaaldosas@gmail.com>
2024-10-27 16:51:40 +0700

    feat: dbt init

---
+++ dbtlearn/models/src/src_reviews.sql
@@ -0,0 +1,13 @@
+WITH reviews_listings AS (
+    SELECT
+        *
+    FROM
+        AIRBNB.RAW.RAW_REVIEWS
+)
+SELECT
+    listing_id,
+    date AS review_date,
+    reviewer_name,
+    comments AS review_text,
+    sentiment AS review_sentiment
+FROM reviews_listings
