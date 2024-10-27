55c51fe0957455af6c74e319f5e3aa5a4677b3de
Rangga <ranggaaldosas@gmail.com>
2024-10-27 17:51:24 +0700

    feat: incremental model

---
+++ dbtlearn/models/dim/dim_hosts_cleansed.sql
@@ -0,0 +1,16 @@
+WITH hosts_listings AS (
+    SELECT
+        *
+    FROM
+        {{ ref('src_hosts') }}
+)
+SELECT
+    host_id,
+    NVL(
+        host_name,
+        'Anonymous'
+    ) AS host_name,
+    is_superhost,
+    created_at,
+    updated_at
+FROM hosts_listings
